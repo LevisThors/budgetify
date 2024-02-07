3f9718036f7169a0d525c3d089b138713b374877
Shukri Mamatsashvili <mamatsashvili.shukri@gmail.com>
2024-02-07 23:15:38 +0400

    CORS setup

--- Backend/budgetify/config/cors.php
+++ Backend/budgetify/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['budgetify-34qwodn1g-levisthors-projects.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
