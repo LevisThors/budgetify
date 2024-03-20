5e33e425a1912e55d1335606cd5ebaf1b928b0a4
Shukri Mamatsashvili <mamatsashvili.shukri@gmail.com>
2024-03-20 23:50:12 +0400

    Added language selector

--- Frontend/budgetify/src/components/partials/User.tsx
+++ Frontend/budgetify/src/components/partials/User.tsx
@@ -8,16 +8,22 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { getCookie } from "cookies-next";
 import Logout from "./Logout";
-import { redirect } from "next/navigation";
+import { redirect, useParams, usePathname, useRouter } from "next/navigation";
 import Image from "next/image";
 import PATHS from "@/paths";
 import { useEffect, useState } from "react";
+import Link from "next/link";
+import { Select, SelectContent, SelectTrigger } from "../ui/select";
+import { SelectItem, SelectValue } from "@radix-ui/react-select";
 
 export default function User() {
     const [userData, setUserData] = useState({
         firstName: "",
         lastName: "",
     });
+    const pathName = usePathname();
+    const params = useParams();
+    const router = useRouter();
 
     const getUserData = async () => {
         try {
@@ -35,6 +41,14 @@ export default function User() {
         }
     };
 
+    const changeLocale = (lang: string) => {
+        const languages = ["en", "ka"];
+        if (params.locale != lang) {
+            console.log("hi");
+            router.push(pathName.replace(params.locale.toString(), lang));
+        }
+    };
+
     useEffect(() => {
         getUserData().then((data) => {
             if (!data.firstName) redirect(PATHS.AUTH.LOGIN);
@@ -43,23 +57,46 @@ export default function User() {
     }, []);
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger className="flex gap-2 items-center outline-none">
-                <span>
-                    <Image
-                        src="/icons/user.svg"
-                        alt="account"
-                        width={22}
-                        height={22}
+        <div className="flex gap-2">
+            <Select>
+                <SelectTrigger className="w-[180px]">
+                    <SelectValue
+                        placeholder={params.locale.toString().toUpperCase()}
                     />
-                </span>
-                {userData.firstName + " " + userData.lastName}
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuItem>
-                    <Logout />
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+                </SelectTrigger>
+                <SelectContent>
+                    <div
+                        className="cursor-pointer"
+                        onClick={() => changeLocale("en")}
+                    >
+                        EN
+                    </div>
+                    <div
+                        className="cursor-pointer"
+                        onClick={() => changeLocale("ka")}
+                    >
+                        KA
+                    </div>
+                </SelectContent>
+            </Select>
+            <DropdownMenu>
+                <DropdownMenuTrigger className="flex gap-2 items-center outline-none">
+                    <span>
+                        <Image
+                            src="/icons/user.svg"
+                            alt="account"
+                            width={22}
+                            height={22}
+                        />
+                    </span>
+                    {userData.firstName + " " + userData.lastName}
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuItem>
+                        <Logout />
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
     );
 }
