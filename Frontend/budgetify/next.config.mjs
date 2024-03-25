05be7fc4c3c017d7747baf47da085e0f37ebe6aa
Shukri Mamatsashvili <mamatsashvili.shukri@gmail.com>
2024-03-25 07:48:17 +0400

    update images

--- Frontend/budgetify/next.config.mjs
+++ Frontend/budgetify/next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "0382-95-137-233-69.ngrok-free.app",
+                hostname: "8820-95-137-233-69.ngrok-free.app",
             },
         ],
     },
